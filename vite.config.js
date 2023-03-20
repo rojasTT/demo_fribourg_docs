53eae3422dc0f67e105116ec6f334deb0e1c6ab1
agauch <78849481+agauch@users.noreply.github.com>
2023-03-20 10:22:06 +0100

    utilisation de dev.topomat.local

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 import * as path from 'path';
 
 export default defineConfig({
@@ -9,11 +10,15 @@ export default defineConfig({
         }
     },
     server: {
+        host: 'dev.topomat.local',
         fs: {
             allow: [ // il faut autoriser vite à chercher des fichiers dans un autre répertoire. Nécessaire pour que les fonts esri (appelées par le css esri dans _maps) se chargent
                 '../../../Develop/_maps_esm',
                 '..'
             ]
         }
-    }
+    },
+    plugins: [mkcert({
+        hosts: ['dev.topomat.local']
+    })]
 });
